f799670f8baeadd9ced4556ca93a0b4b56e27d22
head123123 <130895312+head123123@users.noreply.github.com>
2023-08-07 16:07:51 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from linebot.models import  *
 
 from line_bot_api import *
 from events.basic import *
+from events.oil import *
 
 app= Flask(__name__)
 
@@ -28,11 +29,17 @@ def callback():
 def handle_message(event):
     message_text = str(event.message.text).lower()
 
+    ########### 使用說明 選單 油價查詢 ###########
     if message_text == "@使用說明":
         about_us_event(event)
         Usage(event)
     
-    # if event.message.text == "@即時股價":
+    if event.message.text == "想知道油價":
+        content = oil_price()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content)
+        )
 
         # line_bot_api.reply_message(event.reply_token,buttons_template)
     
