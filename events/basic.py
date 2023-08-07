6fa896cd1d2f997dff4abff586a72b6ae5ed3a5d
head123123 <130895312+head123123@users.noreply.github.com>
2023-08-07 14:08:53 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -32,3 +32,23 @@ Hello! 您好，歡迎您成為 Master Finance 的好友！
     line_bot_api.reply_message(
         event.reply_token,
         [text_message,sticker_message])
+    
+def push_msg(event,msg):
+    try:
+        user_id = event.source.user_id
+        line_bot_api.push_message(user_id,TextSendMessage(text=msg))
+    except:
+        room_id = event.source.room_id
+        line_bot_api.push_message(room_id,TextSendMessage(text=msg))
+
+def Usage(event):
+    push_msg(event,"    😼😼查詢方法😼😼    \
+             \n\
+             \n👍小幫手可以查詢油價、匯率、股價👍\
+             \n\
+             \n❤️油價通知➡➡➡輸入查詢油價❤️\
+             \n🧡匯率通知➡➡➡輸入查詢匯率🧡\
+             \n💛匯率兌換➡➡➡換匯USD/TWD💛\
+             \n💜股價查詢➡➡➡輸入#股票代號💜")
+
+
