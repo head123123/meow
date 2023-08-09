a978566a201708629bc5a97a2ddba91e9aa39df4
head123123 <130895312+head123123@users.noreply.github.com>
2023-08-09 16:00:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from line_bot_api import *
-from events.basic import *
-from events.oil import *
-from events.Msg_Template import *
+from basic import *
+from oil import *
+from Msg_Template import *
 from model.mongodb import *
 import re
 import twstock
@@ -24,26 +24,28 @@ def callback():
 
     return "OK"
 
-#處理訊息
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
-    uid = profile.user_id
+    uid = profile.user_id  # 使用者id
+    user_name = profile.display_name
+    print(user_name)
     message_text = str(event.message.text).lower()
     msg = str(event.message.text).upper().strip()
     emsg = event.message.text
-    user_name=profile.display_name #使用者名稱
 
-    ########### 使用說明 選單 油價查詢 ###########
+    # ############"使用說明"############
     if message_text == "@使用說明":
         about_us_event(event)
         Usage(event)
-    
-    if event.message.text == "想知道油價":
+
+    # ############"油價查詢"############
+    if message_text == "油價查詢":
         content = oil_price()
         line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=content)
+        event.reply_token, 
+        TextSendMessage(content)
         )
 
     # ############"股價查詢"############
@@ -53,34 +55,25 @@ def handle_message(event):
         TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
         )
 
-    #股價查詢
-    if re.match("想知道股價[0-9]" , msg):
-        stockNumber = msg[5:9]
+    if re.match("想知道股價[0-9]", msg):
+        stockNumber = msg[-4:]
         btn_msg = stock_reply_other(stockNumber)
-        line_bot_api.push_message(uid , btn_msg)
-        line_bot_api.push_message(uid,TextSendMessage("in2"))#
+        line_bot_api.push_message(uid, btn_msg)
         return 0
-    #新增使用者關注的股票到mongodb
-    if re.match('關注[0-9]{4}[0-9]',msg):
+    
+    if re.match("關注[0-9]{4}[<>][0-9]", msg):
         stockNumber = msg[2:6]
-        line_bot_api.push_message(uid,TextSendMessage("in"))#
-        line_bot_api.push_message(uid,TextSendMessage("加入股票代號"+stockNumber))
-        content = write_my_stock(uid,user_name,stockNumber,msg[6:7],msg[7:])
-        line_bot_api.push_message(uid,TextSendMessage(content))
-    # else:
-    #     content = write_my_stock(uid,user_name,stockNumber,"未設定","未設定")
-    #     line_bot_api.push_message(uid,TextSendMessage(content))
+        content = write_my_stock(uid, user_name, stockNumber, msg[6:7], msg[7:])
+        line_bot_api.push_message(uid, TextSendMessage(content))
+    else:
+        content = write_my_stock(uid, user_name, stockNumber, "未設定", "未設定")
+        line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
     
-    if (msg.startswith('#')):
-        text = msg[1:]
-        content =''
-        line_bot_api.push_message(uid,TextSendMessage("in3"))#
-    
+
     if (emsg.startswith('#')):
         text = emsg[1:]
-        content =''
-        line_bot_api.push_message(uid,TextSendMessage("in4"))#
+        content = ""
 
         try:
             stock_rt = twstock.realtime.get(text)
