1cf95dfe20aba353fb68ced713fb9df50c9de7e7
head123123 <130895312+head123123@users.noreply.github.com>
2023-08-09 13:38:34 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask,request,abort
-from linebot import(LineBotApi,WebhookHandler,exceptions)
-from linebot.exceptions import (InvalidSignatureError)
-from linebot.models import  *
-
 from line_bot_api import *
 from events.basic import *
 from events.oil import *
+from events.msg_template import *
+
+from model.mongodb import *
+import re
+import twstock
+import datetime
 
 app= Flask(__name__)
 
@@ -30,6 +31,8 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     uid = profile.user_id
     message_text = str(event.message.text).lower()
+    msg = str(event.message.text).upper().strip()
+    emsg = event.message.text
 
     ########### 使用說明 選單 油價查詢 ###########
     if message_text == "@使用說明":
@@ -43,28 +46,105 @@ def handle_message(event):
             TextSendMessage(text=content)
         )
 
-    #### 股票區 ####
-    if event.message.text == "股價查詢":
-        line_bot_api.push_message(uid,TextSendMessage("請輸入#加股票代號..."))
+    # ############"股價查詢"############
+    if message_text == "股價查詢":
+        line_bot_api.push_message(
+        uid, 
+        TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
+        )
+
+    #股價查詢
+    if re.match("想知道股價[0-9]" , msg):
+        stockNumber = msg[5:9]
+        btn_msg = stock_reply_other(stockNumber)
+        line_bot_api.push_message(uid , btn_msg)
+        return 0
+    #新增使用者關注的股票到mongodb
+    if re.match('關注[0-9]{4}[0-9]',msg):
+        stockNumber = msg[2:6]
+        content = write_my_stock(uid,user_name,stockNumber,msg[6:7],msg[7:])
+        line_bot_api.push_message(uid,TextSendMessage(content))
+    else:
+        content = write_my_stock(uid,user_name,stockNumber,"未設定","未設定")
+        line_bot_api.push_message(uid,TextSendMessage(content))
+        return 0
+    
+    if (emsg.startswith('#')):
+        text = emsg[1:]
+        content =''
+
+        stock_rt = twstock.realtime.get(text)
+        my_datetime = datetime.datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
+        my_time = my_datetime.strftime('%H:%M:%S')
+
+        content +='%s (%s) %s\n' % (
+            stock_rt['info']['name'],
+            stock_rt['info']['code'],
+            my_time)
+        
+        content += '現價: %s / 開盤: %s\n'%(
+            stock_rt['realtime']['latest_trade_price'],
+            stock_rt['realtime']['open'])
+        
+        content += '最高: %s / 最低:%s\n'%(
+            stock_rt['realtime']['high'],
+            stock_rt['realtime']['low'])
+        
+        content += '量: %s\n'%(stock_rt['realtime']['accumulate_trade_volume'])
+
+        stock = twstock.Stock(text)
+        content += '-----\n'
+        content += '最近五日價格: \n'
+        price5 = stock.price[-5:][::-1]
+        date5 = stock.date[-5:][::-1]
+        for i in range(len(price5)):
+            content += '[%s] %s\n' % (date5[i].strftime("%Y-%m-%d"), price5[i])
+        line_bot_api.reply_message(
+            event.reply_token, 
+            TextSendMessage(text=content)
+        )
+
+     # ############"@小幫手"############
+    if message_text == "@小幫手":
+        button_template = ButtonsTemplate()
+        line_bot_api.reply_message(
+        event.reply_token, button_template
+        )
+
+        
 
 
 @handler.add(FollowEvent)
 def handle_follow(event):
-    welcome_msg = '''Hello! 您好，歡迎您成為 Master Finance 的好友！
-                                   
-我是Master 財經小幫手
-                                   
--這裡有股票、匯率資訊哦～
--直接點選下方[圖中]選單功能
-                                   
-期待您的光臨'''
+    emojis = [
+        {
+            "index": 0, 
+            "productId": "5ac21a18040ab15980c9b43e", 
+            "emojiId": "009"
+        }, 
+        {
+            "index": 16, 
+            "productId": "5ac21a18040ab15980c9b43e", 
+            "emojiId": "014"
+        }
+    ]
+
+    welcome_message = TextSendMessage(text='''$ Agave Finance $
+    您好，歡迎加入成為 Agave Finance 的好友!!!
+    我是Agave財經小幫手~
+    下方選單有：
+    股票查詢、油價查詢、匯率查詢、自動提醒、資訊整理、使用說明
+    使用上有任何問題可以參考使用說明''', emojis=emojis)
+
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=welcome_msg))
+        event.reply_token, welcome_message
+        )
     
+
 @handler.add(UnfollowEvent)
-def handle_unfollow(event):
+def hadle_unfollow(event):
     print(event)
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     app.run()
